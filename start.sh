10c186e80a460e52435ce0f74586ec1d8db9364e
Adam Kelly <adam@cthulahoops.org>
2025-07-13 10:18:02 +0100

    Simplify start up - no need to mess with env.

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Generate initial calendar
-BRENT_PROPERTY_ID=$BRENT_PROPERTY_ID uv run python waste_collection_scraper.py -o /site/dist/calendar.ics
-
-# Start HTTP server
+uv run python waste_collection_scraper.py -o /site/dist/calendar.ics
 python -m http.server 8080 -d /site/dist
